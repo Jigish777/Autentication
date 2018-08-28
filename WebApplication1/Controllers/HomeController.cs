523092d5f1962e4850a7ff078ac2e7d3bb81b11d
jack mack <jigish1991@gmail.com>
2018-08-29 00:33:47 +0530

    Primay commit for basic project

---
+++ WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public HomeController()
+        {
+
+        }
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [CustomAuthorizeAttribute]
+        public ActionResult Profile()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        [ValidateAntiForgeryToken]
+        [HttpPost]
+        public RedirectResult Login(string UserName,string Password)
+        {
+            if (UserName == "Jigish" && Password == "Desai")
+            {
+                FormsAuthentication.SetAuthCookie(UserName, true);
+                return Redirect("/Home/Profile");
+            }
+            return Redirect("/Home/Index");
+        }
+
+
+    }
+}
---
+++ WebApplication1/Filters/CustomAuthorize.cs
@@ -0,0 +1,24 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+public class CustomAuthorizeAttribute : AuthorizeAttribute
+{
+    
+    
+    protected override bool AuthorizeCore(HttpContextBase httpContext)
+    {
+        if (httpContext.User.Identity.IsAuthenticated)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+        
+    }
+    protected override void HandleUnauthorizedRequest(AuthorizationContext filterContext)
+    {
+        filterContext.Result = new HttpUnauthorizedResult();
+    }
+}
